aac51759b2b6b542b95d206b55924b62121ffe09
Kristen <kristenmcgarry1@gmail.com>
2017-05-02 15:23:21 -0400

    completed park model

---
+++ lung-cancer-risk-models/park/README.md
@@ -0,0 +1,62 @@
+# Park, Individualized Risk Prediction Model for Lung Cancer in Korean Men (Park et al, 2013)
+Created: May 2, 2017
+
+
+### Objective
+This knowledge object (KO) predicts an individuals lung cancer risk within 8 years. This predictor is targeted for Korean mean. The calculation is from Appendix S1 in Park et al, 2013.
+
+### Description
+The input is as follows: {"age":0,"smokerStatus":0,"asi":0,"bmi":0,"physActiv":0,"fastingGluc":0})
+- age
+- smokerStatus
+    - 0 = never smoker
+    - 1 = past smoker
+    - 2 = current smoker, <0.5 pack/day
+    - 3 = current smoker, 0.5-0.99 pack/day
+    - 4 = current smoker, >=1 pack/day
+- asi (age at smoking initiation)
+    - 0 age >= 40 at smoking initiation
+    - 1 30<= age <40 at smoking initiation
+    - 2 19<= age <30 at smoking initiation
+    - 3 16<= age <19 at smoking initiation
+    - 4 age <16 at smoking initiation
+- bmi
+    - 0 BMI 18.5–22.9
+    - 1 BMI < 18.5
+    - 2 BMI 23.0–24.9
+    - 3 BMI ≥ 25.0
+- physActiv (physical activity)
+    - 0 none
+    - 1 light
+    - 2 moderate
+    - 3 heavy
+- fastingGluc (fasting glucose)
+    - 0 glucose < 126
+    - 1 glucose >= 126
+
+
+### Running
+The script can be run by calling execute({"age":50,"smokerStatus":3,"asi":1,"bmi":2,"physActiv":1,"fastingGluc":1}). We have also included a test() function that we suggest running before running your data through.
+
+
+### Getting started
+To run program from terminal, go to terminal and cd into the directory where the python file is located. Enter the following:
+1. python
+2. import park
+3. park.execute({"age":50,"smokerStatus":3,"asi":1,"bmi":2,"physActiv":1,"fastingGluc":1})
+
+### Running through SHELF REST API...
+To run through SHELF REST API, the knowledge object needs to be added to ObjectTeller and to the REST API SHELF before its executable.
+The ark ID for this object is: **ark:/99999/fk4r49xd2g**
+
+1. To add to shelf: {{baseUrl}}/shelf/ark:/99999/fk4r49xd2g
+2. To check shelf: {{baseUrl}}/shelf
+3. To execute:
+   - Add the following headers:
+     - Content-Type:application/json
+     - Accept:application/json
+   - Enter input into "body" (i.e. {"age":50,"smokerStatus":3,"asi":1,"bmi":2,"physActiv":1,"fastingGluc":1})
+   - Run the POST command. {{baseUrl}}/knowledgeObject/ark:/99999/fk4r49xd2g/result
+
+### Literature
+The documentation can be found at: http://journals.plos.org/plosone/article?id=10.1371/journal.pone.0054823
--- lung-cancer-risk-models/park/park.py
+++ lung-cancer-risk-models/park/park.py
@@ -1,15 +1,64 @@
 # Park, Individualized Risk Prediction Model for Lung Cancer in Korean Men
 # Author: Kristen McGarry
-# Last Updated: April 26th, 2017
+# Last Updated: May 2, 2017
+from math import exp
+
+def execute(inputs):
+    age = inputs["age"]
+    smokerStatus = inputs["smokerStatus"]
+    asi = inputs["asi"]
+    bmi = inputs["bmi"]
+    physActiv = inputs["physActiv"]
+    fastingGluc = inputs["fastingGluc"]
+
+    # Predicting the probability of developing lung cancer within 8 years; equation on page 4
+    A = calcA(age,smokerStatus,asi,bmi,physActiv,fastingGluc)
+    E = exp(A)
+    # for 8 years...
+    P = 1-(0.9983894078**E)
+    P_percent = round((P*100),2)
+    interpretation ="This individual has a " +str(P_percent) +"% probability of developing lung cancer within 8 years."
+    return {"result":P_percent,"interpretation":interpretation}
 
 
-# beta values for risk factors, index correlates with risk factor order shown in table 3. (i.e. betas["smoke"][0] = never smoked, betas["smoke"][1] = past smoker)
-betas = {
-  "meanAge": [0.1668],
-  "meanAgeSq": [-0.002],
-  "smokerStatus": [0, 0.418, 0.4444, 0.9414, 1.3889],
-  "ageStartSmok":[0,0.2194,0.2809,0.5249,0.7120],
-  "bmi":[0.3306,0,-0.2468,-0.3386],
-  "physActiv": [0,-0.0909,-0.1412],
-  "fastingGluc": [0,0.0792]
-}
+def calcA(age,smokerStatus,asi,bmi,physActiv,fastingGluc):
+    meanAge = 45
+    ageSq = (age-meanAge)**2
+    ageA = (0.1668*((age-meanAge)-0)) -(0.0020*(ageSq-107.3007))
+
+    binValues = []
+    smokerTemp = [0,0,0,0]
+    if smokerStatus != 0:
+        smokerTemp[smokerStatus-1] = 1
+
+    asiTemp = [0,0,0,0]
+    if asi != 0:
+        asiTemp[asi-1] = 1
+
+    bmiTemp = [0,0,0]
+    if bmi != 0:
+        bmiTemp[bmi-1] = 1
+
+    phaTemp = [0,0,0]
+    if physActiv != 0:
+        phaTemp[physActiv-1] = 1
+
+    if fastingGluc == 1:
+        gluTemp = [1]
+    else:
+        gluTemp = [0]
+
+    # order = smoker status, asi, bmi, pha, glu
+    # binValues = binary values
+    binValues = smokerTemp + asiTemp + bmiTemp + phaTemp + gluTemp
+    A = ageA  + (0.4180*((binValues[0]) - 0.1511))  + (0.4444*((binValues[1]) - 0.0905)) + (0.9414*((binValues[2]) - 0.3330)) + (1.3889*((binValues[3]) - 0.1390))  + (0.2194*((binValues[4]) - 0.1441)) + (0.2809*((binValues[5]) - 0.3570)) + (0.5249*((binValues[6]) - 0.0164)) + (0.7120*((binValues[7]) - 0.0064)) + (0.3306*((binValues[8]) - 0.0239))  - (0.2468*((binValues[9]) - 0.2837)) - (0.3386*((binValues[10]) - 0.2851)) - (0.0909*((binValues[11]) - 0.1590)) - (0.1412*((binValues[12]) -0.2952))-(0.0521*((binValues[13]) - 0.0676))+(0.0792*(((binValues[14]) - 0.0605)))
+
+    return A
+
+def test():
+    if execute({"age":50,"smokerStatus":3,"asi":1,"bmi":2,"physActiv":1,"fastingGluc":1}) != {'interpretation': 'This individual has a 0.63% probability of developing lung cancer within 8 years.', 'result': 0.63}:
+        return "error."
+    if execute({"age":0,"smokerStatus":0,"asi":0,"bmi":0,"physActiv":0,"fastingGluc":0}) != {'interpretation': 'This individual has a 0.0% probability of developing lung cancer within 8 years.', 'result': 0.0}:
+        return "error."
+    else:
+        return "ok."
