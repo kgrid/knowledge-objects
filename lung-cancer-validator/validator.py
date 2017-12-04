5973c2330e69b9220e88d6636617ace5ba24de7d
Natalie Lampa <nlampa@umich.edu>
2017-12-04 18:06:56 -0500

    now reads in excel file with patient data and writes risk scores to another workbook

--- lung-cancer-validator/validator.py
+++ lung-cancer-validator/validator.py
@@ -7,8 +7,7 @@
 #
 
 import requests
-import xlrd
-import json
+import xlwt, xlrd, json
 
 #base url for using kgrid server activator
 url = "http://kgrid.med.umich.edu/stack/knowledgeObject/ark:/99999"
@@ -59,89 +58,34 @@ def park(park_age, park_smok_status, park_asi, park_bmi, park_phys_activ, park_f
 
 #
 #
-#	Main
+## This main creates a new workbook for the patient risk scores
 #
 #
-
-
-def main():
-
-    book = xlrd.open_workbook("three_model_excel_template.xlsx")
-    sh = book.sheet_by_index(0)
-
-	while (sh.rows)
-		#row by row iteration
-		#assign all variables, call all model functions 
-		# id = a.value
-
-		#bach model
-		bach_age = b.value
-		bach_cpd = c.value
-		bach_yrs_smok = d.value
-		bach_yrs_quit = e.value
-		bach_asbestos = f.value
-		bach_sex = g.value #0 male, 1 female 
-		bach_quit = h.value #0 no, 1 yes
-
-		#marcus model
-		marcus_age = i.value
-		marcus_sex = j.value
-		marcus_smok_durat = k.value
-		marcus_copd = l.value
-		marcus_prior_diag = m.value
-		marcus_early_onset = n.value
-		marcus_late_onset = o.value
-
-		#park model
-		park_age = p.value
-		park_smok_status = q.value
-		park_asi = r.value
-		park_bmi = s.value
-		park_phys_activ = t.value
-		park_fasting_gluc = u.value
-
-		# create new sheet, destination for results
-		newsheet = three_model_excel_template.add_sheet('Model Results')
-
-		#Bach
-		for r in range(sheet.nrows):
-			value = bach(r.a, r.b, bach_yrs_smok, bach_yrs_quit, bach_asbestos, bach_sex, 
-			bach_quit)
-
-       		newsheet.write(r, 0, value)
-
-		#Marcus
-		for r in range(sheet.nrows):
-       		newsheet.write(r, 1, sheet.cell_value(r, 1))
-
-
-		v.value = bach(bach_age, bach_cpd, bach_yrs_smok, bach_yrs_quit, bach_asbestos, bach_sex, 
-		bach_quit)
-
-		w.value = marcus(marcus_age, marcus_sex, marcus_smok_durat, marcus_copd, marcus_prior_diag,
-		marcus_early_onset, marcus_late_onset)
-
-		x.value = park(park_age, park_smok_status, park_asi, park_bmi, park_phys_activ, park_fasting_gluc)
-
-
-
-
-main()
-
-
-## new main
 
 def main():
 
 	# opens a workbook
-	workbook = xlrd.open_workbook(filename = 'three_model_excel_template.xlsx')
+	workbook_in = xlrd.open_workbook(filename = 'three_model_excel_template.xlsx')
 
-	worksheet = workbook.sheet_by_index(0)
+	worksheet = workbook_in.sheet_by_index(0)
 	
 	current_row = 1
 	col_index = 0
+	# col_index_new_ws = 0
+
+	# creates new workbook for the patient data risk scores
+	new_workbook = xlwt.Workbook()
+	scores_ws = new_workbook.add_sheet('Patient Risk Scores')
+
+	scores_ws.write(0, 0, "id")
+	scores_ws.write(0, 1, "bach risk score")
+	scores_ws.write(0, 2, "marcus risk score")
+	scores_ws.write(0, 3, "park risk score")
 
 	while (current_row < int(worksheet.nrows)):
+		# writes patient id to the new patient risk scores worksheet 
+		scores_ws.write(current_row, col_index, int(worksheet.cell(current_row, col_index).value)) 
+
 		bach_age = int(worksheet.cell(current_row, col_index + 1).value)
 		bach_cpd = int(worksheet.cell(current_row, col_index + 2).value)
 		bach_yrs_smok = int(worksheet.cell(current_row, col_index + 3).value)
@@ -165,18 +109,97 @@ def main():
 		park_phys_activ = int(worksheet.cell(current_row, col_index + 19).value)
 		park_fasting_gluc = int(worksheet.cell(current_row, col_index + 20).value)
 
-		bach_json = bach(bach_age, bach_cpd, bach_yrs_smok, bach_yrs_quit, bach_asbestos, bach_sex, bach_quit)
-		marcus_json = marcus(marcus_age, marcus_sex, marcus_smok_durat, marcus_copd, marcus_prior_diag, marcus_early_onset, marcus_late_onset)
-		park_json = park(park_age, park_smok_status, park_asi, park_bmi, park_phys_activ, park_fasting_gluc)
-		# pretty prints results of each json object
-		print json.dumps(bach_json['result'], indent=4, sort_keys=True)
-		print json.dumps(marcus_json['result'], indent=4, sort_keys=True)
-		print json.dumps(park_json['result'], indent=4, sort_keys=True)
+		bach_score = bach(bach_age, bach_cpd, bach_yrs_smok, bach_yrs_quit, bach_asbestos, bach_sex, bach_quit)
+		marcus_score = marcus(marcus_age, marcus_sex, marcus_smok_durat, marcus_copd, marcus_prior_diag, marcus_early_onset, marcus_late_onset)
+		park_score = park(park_age, park_smok_status, park_asi, park_bmi, park_phys_activ, park_fasting_gluc)
+
+		# prints the risk scores
+		print bach_score
+		print marcus_score
+		print park_score
+
+		scores_ws.write(current_row, col_index + 1, bach_score)
+		scores_ws.write(current_row, col_index + 2, marcus_score)
+		scores_ws.write(current_row, col_index + 3, park_score)
 
 		current_row = current_row + 1
+
+	new_workbook.save('PatientRiskScores.xls')
 
 main()
 
 
+
+
+#
+#
+#	Main
+#
+#
+
+
+# def main():
+
+#     book = xlrd.open_workbook("three_model_excel_template.xlsx")
+#     sh = book.sheet_by_index(0)
+
+# 	while (sh.rows)
+# 		#row by row iteration
+# 		#assign all variables, call all model functions 
+# 		# id = a.value
+
+# 		#bach model
+# 		bach_age = b.value
+# 		bach_cpd = c.value
+# 		bach_yrs_smok = d.value
+# 		bach_yrs_quit = e.value
+# 		bach_asbestos = f.value
+# 		bach_sex = g.value #0 male, 1 female 
+# 		bach_quit = h.value #0 no, 1 yes
+
+# 		#marcus model
+# 		marcus_age = i.value
+# 		marcus_sex = j.value
+# 		marcus_smok_durat = k.value
+# 		marcus_copd = l.value
+# 		marcus_prior_diag = m.value
+# 		marcus_early_onset = n.value
+# 		marcus_late_onset = o.value
+
+# 		#park model
+# 		park_age = p.value
+# 		park_smok_status = q.value
+# 		park_asi = r.value
+# 		park_bmi = s.value
+# 		park_phys_activ = t.value
+# 		park_fasting_gluc = u.value
+
+# 		# create new sheet, destination for results
+# 		newsheet = three_model_excel_template.add_sheet('Model Results')
+
+# 		#Bach
+# 		for r in range(sheet.nrows):
+# 			value = bach(r.a, r.b, bach_yrs_smok, bach_yrs_quit, bach_asbestos, bach_sex, 
+# 			bach_quit)
+
+#        		newsheet.write(r, 0, value)
+
+# 		#Marcus
+# 		for r in range(sheet.nrows):
+#        		newsheet.write(r, 1, sheet.cell_value(r, 1))
+
+
+# 		v.value = bach(bach_age, bach_cpd, bach_yrs_smok, bach_yrs_quit, bach_asbestos, bach_sex, 
+# 		bach_quit)
+
+# 		w.value = marcus(marcus_age, marcus_sex, marcus_smok_durat, marcus_copd, marcus_prior_diag,
+# 		marcus_early_onset, marcus_late_onset)
+
+# 		x.value = park(park_age, park_smok_status, park_asi, park_bmi, park_phys_activ, park_fasting_gluc)
+
+
+
+
+# main()
 
 
