7e0743311100f968109fadad3a805b209699cda8
nlpa <nlampa@umich.edu>
2018-02-02 14:04:11 -0500

    updated validator

--- lung-cancer-validator/validator.py
+++ lung-cancer-validator/validator.py
@@ -6,6 +6,7 @@
 #
 #
 
+
 import requests
 import xlwt, xlrd, json
 
@@ -15,7 +16,7 @@ url = "http://kgrid.med.umich.edu/stack/knowledgeObject/ark:/99999"
 headers = {'content-type': "application/json"}
 
 #model specific urls
-tammemagi_url = url + "/fk4x92gk0r/result" # why is this the same as marcus
+tammemagi_url = url + "/fk4jh3tk9s/result"
 cassidy_url = url + "/fk4571pp25/result"
 
 
@@ -27,50 +28,50 @@ def tammemagi(tam_age, tam_edLevel, tam_bmi, tam_copd, tam_hxLungCancer, tam_fam
 	response = requests.post(tammemagi_url, data=json.dumps(payload), headers=headers)
 	tammemagi_data = json.loads(response.text)
 
-	return(tammemagi_data['result']['result'])
-
+	return(tammemagi_data)
 
 def cassidy(cassidy_sex, cassidy_age, cassidy_pneum, cassidy_asbestos, cassidy_cancHx, cassidy_famHxCanc_early, cassidy_famHxCanc_late,
 			cassidy_smokDur1to20, cassidy_smokDur21to40, cassidy_smokDur41to60, cassidy_smokeDur60plus):
-	unfortunate_list = []
+	risk_factors = []
 	if cassidy_pneum != "":
-		unfortunate_list.append(cassidy_pneum)
+		risk_factors.append(cassidy_pneum)
 
 	if cassidy_asbestos != "":
-		unfortunate_list.append(cassidy_asbestos)
+		risk_factors.append(cassidy_asbestos)
 
 	if cassidy_cancHx != "":
-		unfortunate_list.append(cassidy_cancHx)
+		risk_factors.append(cassidy_cancHx)
 
 	if cassidy_famHxCanc_early != "":
-		unfortunate_list.append(cassidy_famHxCanc_early)
+		risk_factors.append(cassidy_famHxCanc_early)
 
 	if cassidy_famHxCanc_late != "":
-		unfortunate_list.append(cassidy_famHxCanc_late)
+		risk_factors.append(cassidy_famHxCanc_late)
 
 	if cassidy_smokDur1to20 != "":
-		unfortunate_list.append(cassidy_smokDur1to20)
+		risk_factors.append(cassidy_smokDur1to20)
 
 	if cassidy_smokDur21to40 != "":
-		unfortunate_list.append(cassidy_smokDur21to40)
+		risk_factors.append(cassidy_smokDur21to40)
 
 	if cassidy_smokDur41to60 != "":
-		unfortunate_list.append(cassidy_smokDur41to60)
+		risk_factors.append(cassidy_smokDur41to60)
 
 	if cassidy_smokeDur60plus != "":
-		unfortunate_list.append(cassidy_smokeDur60plus)
+		risk_factors.append(cassidy_smokeDur60plus)
 
-	print type(unfortunate_list[0])
-	print unfortunate_list
-
-	payload = {"sex":cassidy_sex,"age":cassidy_age,"riskFactors":unfortunate_list}
+	payload = {"sex":cassidy_sex,"age":cassidy_age,"riskFactors":risk_factors}
 
 	response = requests.post(cassidy_url, data=json.dumps(payload), headers=headers)
 	cassidy_data = json.loads(response.text)
 
 	return(cassidy_data['result']['result'])
 
-
+#
+#
+## This creates a new workbook for the patient risk scores
+#
+#
 def main():
 
 	# opens a workbook
@@ -80,83 +81,59 @@ def main():
 	
 	current_row = 1
 	col_index = 0
-	# col_index_new_ws = 0
 
 	# creates new workbook for the patient data risk scores
 	new_workbook = xlwt.Workbook()
 	scores_ws = new_workbook.add_sheet('Patient Risk Scores')
 
 	scores_ws.write(0, 0, "id")
-	#scores_ws.write(0, 4, "tammemagi risk score")
-	scores_ws.write(0, 8, "cassidy risk score")
+	scores_ws.write(0, 1, "tammemagi risk score")
+	scores_ws.write(0, 2, "cassidy risk score")
 
 	while (current_row < int(worksheet.nrows)):
 		# writes patient id to the new patient risk scores worksheet 
 		scores_ws.write(current_row, col_index, int(worksheet.cell(current_row, col_index).value)) 
 
-		#tam_age = int(worksheet.cell(current_row, col_index + 21).value)
-		#tam_edLevel = int(worksheet.cell(current_row, col_index + 22).value)
-		#tam_bmi = int(worksheet.cell(current_row, col_index + 23).value)
-		#tam_copd = int(worksheet.cell(current_row, col_index + 24).value)
-		#tam_hxLungCancer = int(worksheet.cell(current_row, col_index + 25).value)
-		#tam_famHxCanc = int(worksheet.cell(current_row, col_index + 26).value)
-		#tam_race = int(worksheet.cell(current_row, col_index + 27).value)
-		#tam_cigsPerDay = int(worksheet.cell(current_row, col_index + 28).value)
-		#tam_smokDurat = int(worksheet.cell(current_row, col_index + 29).value)
-		#tam_yrsQuit = int(worksheet.cell(current_row, col_index + 30).value)
+		# read in the variable data from Excel cells
 
+		tam_age = int(worksheet.cell(current_row, col_index + 1).value)
+		tam_edLevel = int(worksheet.cell(current_row, col_index + 2).value)
+		tam_bmi = int(worksheet.cell(current_row, col_index + 3).value)
+		tam_copd = int(worksheet.cell(current_row, col_index + 4).value)
+		tam_hxLungCancer = int(worksheet.cell(current_row, col_index + 5).value)
+		tam_famHxCanc = int(worksheet.cell(current_row, col_index + 6).value)
+		tam_race = int(worksheet.cell(current_row, col_index + 7).value)
+		tam_cigsPerDay = int(worksheet.cell(current_row, col_index + 8).value)
+		tam_smokDurat = int(worksheet.cell(current_row, col_index + 9).value)
+		tam_yrsQuit = int(worksheet.cell(current_row, col_index + 10).value)
 
-		cassidy_sex = "male"
-		#print str(worksheet.cell(current_row, col_index + 89).value)
-		cassidy_age = 67
-		cassidy_pneum = ""
-		cassidy_asbestos = "asbestos"
-		cassidy_cancHx = "cancHx"
-		cassidy_famHxCanc_early = "famHxCanc, early onset"
-		cassidy_famHxCanc_late = ""
-		cassidy_smokDur1to20 = ""
-		cassidy_smokDur21to40 = ""
-		cassidy_smokDur41to60 = ""
-		cassidy_smokeDur60plus = ""
+		cassidy_sex = str(worksheet.cell(current_row, col_index + 11).value)
+		cassidy_age = int(float(worksheet.cell(current_row, col_index + 12).value))
+		cassidy_pneum = str(worksheet.cell(current_row, col_index + 13).value)
+		cassidy_asbestos = str(worksheet.cell(current_row, col_index + 14).value)
+		cassidy_cancHx = str(worksheet.cell(current_row, col_index + 15).value)
+		cassidy_famHxCanc_early = str(worksheet.cell(current_row, col_index + 16).value)
+		cassidy_famHxCanc_late = str(worksheet.cell(current_row, col_index + 17).value)
+		cassidy_smokDur1to20 = str(worksheet.cell(current_row, col_index + 18).value)
+		cassidy_smokDur21to40 = str(worksheet.cell(current_row, col_index + 19).value)
+		cassidy_smokDur41to60 = str(worksheet.cell(current_row, col_index + 20).value)
+		cassidy_smokeDur60plus = str(worksheet.cell(current_row, col_index + 21).value)
 
+		cassidy_score = cassidy(cassidy_sex, cassidy_age, cassidy_pneum, cassidy_asbestos, cassidy_cancHx, cassidy_famHxCanc_early, 
+			cassidy_famHxCanc_late, cassidy_smokDur1to20, cassidy_smokDur21to40, cassidy_smokDur41to60, cassidy_smokeDur60plus)
+		tammemagi_score = tammemagi(tam_age, tam_edLevel, tam_bmi, tam_copd, tam_hxLungCancer, tam_famHxCanc, tam_race, 
+			tam_cigsPerDay, tam_smokDurat, tam_yrsQuit)
 
-		#cassidy_age = int(worksheet.cell(current_row, col_index + 90).value)
-		#cassidy_age = float(cassidy_age)
-		#cassidy_age = int(cassidy_age)
-		#cassidy_pneum = str(worksheet.cell(current_row, col_index + 91).value)
-		#cassidy_asbestos = str(worksheet.cell(current_row, col_index + 92).value)
-		#cassidy_cancHx = str(worksheet.cell(current_row, col_index + 93).value)
-		#cassidy_famHxCanc_early = str(worksheet.cell(current_row, col_index + 94).value)
-		#cassidy_famHxCanc_late = str(worksheet.cell(current_row, col_index + 95).value)
-		#cassidy_smokDur1to20 = str(worksheet.cell(current_row, col_index + 96).value)
-		#cassidy_smokDur21to40 = str(worksheet.cell(current_row, col_index + 97).value)
-		#cassidy_smokDur41to60 = str(worksheet.cell(current_row, col_index + 98).value)
-		#cassidy_smokeDur60plus = str(worksheet.cell(current_row, col_index + 99).value)
-
-		bach_score = bach(bach_age, bach_cpd, bach_yrs_smok, bach_yrs_quit, bach_asbestos, bach_sex, bach_quit)
-		marcus_score = marcus(marcus_age, marcus_sex, marcus_smok_durat, marcus_copd, marcus_prior_diag, marcus_early_onset, marcus_late_onset)
-		park_score = park(park_age, park_smok_status, park_asi, park_bmi, park_phys_activ, park_fasting_gluc)
-
-		#tammemagi_score = tammemagi()
-			#tam_age, tam_edLevel, tam_bmi, tam_copd, tam_hxLungCancer, tam_famHxCanc, tam_race, tam_cigsPerDay, tam_smokDurat, tam_yrsQuit)
-		spitz_score = spitz(spitz_sex, spitz_age, spitz_smokerStatus, spitz_ets, spitz_emphysema, spitz_hayFever, spitz_dust,
-			spitz_fumes, spitz_chemicals, spitz_asbestos, spitz_pesticides, spitz_woodDust, spitz_asthma, spitz_famHxCanc, 
-			spitz_famHxSmokeCanc, spitz_asi, spitz_ageQuit, spitz_asc, spitz_yrsCess, spitz_packYrs)
-		cassidy_score = cassidy(cassidy_sex, cassidy_age, cassidy_pneum, cassidy_asbestos, cassidy_cancHx, cassidy_famHxCanc_early, cassidy_famHxCanc_late,
-			cassidy_smokDur1to20, cassidy_smokDur21to40, cassidy_smokDur41to60, cassidy_smokeDur60plus)
-
-		# prints the risk scores
-		#print tammemagi_score
+		# prints the risk scores (visual test - REMOVE later)
+		print tammemagi_score
 		print cassidy_score
 
-
-		#scores_ws.write(current_row, col_index + 4, tammemagi_score)
-		scores_ws.write(current_row, col_index + 8, cassidy_score)
+		# writes scores to new woorksheet
+		scores_ws.write(current_row, col_index + 1, tammemagi_score)
+		scores_ws.write(current_row, col_index + 2, cassidy_score)
 
 		current_row = current_row + 1
 
 	new_workbook.save('PatientRiskScores.xls')
 
 main()
-
-
