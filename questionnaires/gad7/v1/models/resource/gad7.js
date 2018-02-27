c4f9383f3ea82a3dfa43fb266c4abd606b5f1d68
Andrew Cullen <drewcullen@Andrews-MacBook-Pro.local>
2018-02-26 21:06:31 -0500

    working on scoring and response functions

--- questionnaires/gad7/v1/models/resource/gad7.js
+++ questionnaires/gad7/v1/models/resource/gad7.js
@@ -1,17 +1,44 @@
-var questionnaire = function()
+var Questionnaire = require('./questionnaire.js');
+
+var gad7 = new Questionnaire("matrix")
+gad7.jsonBuilder().build(
+    "Generalized Anxiety Disorder 7-item (GAD-7) scale",
+
+    "Over the last two weeks, how often have you been bothered by the following problems?",
+
+    [
+        "Feeling nervous, anxious, or on edge",
+        "Not being able to stop or control worrying",
+        "Worrying too much about different things",
+        "Trouble relaxing",
+        "Being so restless that it's hard to sit still",
+        "Becoming easily annoyed or irritable",
+        "Feeling afraid as if something awful might happen"
+    ],
+
+    [0, 3],
+
+    [
+        "Not at all sure",
+        "Several days",
+        "Over half the days",
+        "Nearly every day"
+    ]
+)
+
+function question()
 {
-    return {
-        "prompt": "over the last 2 weeks, how often have you been bothered by the following problems?",
-        
-    }
+    return gad7.getJson();
 }
 
-function score()
+
+
+function score(answers)
 {
 
 }
 
-function interpretation()
+function interpret()
 {
-
+    
 }
--- questionnaires/gad7/v1/models/resource/questionnaire.js
+++ questionnaires/gad7/v1/models/resource/questionnaire.js
@@ -1,24 +1,57 @@
 var Questionnaire = (function ()
 {
 
-
-    function matrixQuestionnaireBuilder(prompt, questions, range, rangeLabels)
-    {
-        console.log(prompt, questions, range, rangeLabels)
-        this.x = prompt + questions
-    }
-
-
+    /**
+     * Questionnaire class
+     * 
+     * @param {String} type 
+     */
     function Questionnaire(type)
     {
-        if(type === "matrix")
-            Questionnaire.prototype.buildQuestionsJson = matrixQuestionnaireBuilder;
+        this.type = type
+
+        /**
+         * 
+         * @param {String} title title of questionnaire
+         * @param {String} prompt Question prompt
+         * @param {Array} questions array of questions
+         * @param {Array} range range of options (ie 1-5 means possible options for each questions is a number from 1 to 5)
+         * @param {Array} rangeLabels labels for the columns
+         */
+        this.matrixQuestionnaireBuilder = (title, prompt, questions, range, rangeLabels) =>
+        {
+            this.json = {
+                type: "uniform matrix",
+                title: title,
+                prompt: prompt,
+                questions: questions,
+                range_labels: rangeLabels,
+                score_range: range
+            }
+        }
+    }
+
+    Questionnaire.prototype.jsonBuilder = function()
+    {
+        var builderFunction = new Function();
+        if(this.type === "matrix")
+            builderFunction = this.matrixQuestionnaireBuilder;
+        if(this.type === "json")
+        
+            
+
+        return {
+            build: builderFunction
+        }
+    }
+
+    Questionnaire.prototype.getJson = function()
+    {
+        return this.json
     }
 
     return Questionnaire;
-})()
+})();
 
 
-var q = new Questionnaire("matrix");
-q.buildQuestionsJson("sup", "a, b, c", "1-5", "bruh, bruh");
-console.log(q.x)
+module.exports = Questionnaire;
