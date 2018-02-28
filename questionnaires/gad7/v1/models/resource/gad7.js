a8b48be958db74c72e662dad5963573c68342a22
Andrew Cullen <drewcullen@Andrews-MacBook-Pro.local>
2018-02-27 22:24:56 -0500

    added scoring and interpreting

--- questionnaires/gad7/v1/models/resource/gad7.js
+++ questionnaires/gad7/v1/models/resource/gad7.js
@@ -1,8 +1,23 @@
 var Questionnaire = require('./questionnaire.js');
 
-var gad7 = new Questionnaire("matrix", function()
+var answers = {
+    answers: [
+        "not at all",
+        "sometimes",
+        "..."
+    ]
+}
+
+var gad7 = new Questionnaire("matrix", function(answersJson)
 {
-    console.log("suh");
+    var answers = answersJson.answers;
+    var sum = 0;
+    for(var i = 0; i < answers.length; i += 1)
+    {
+        sum += gad7.getAnswerScore(i, answers[i])
+    }
+    return sum;
+
 }, function()
 {
 
@@ -58,13 +73,32 @@ function question()
 }
 
 
-
-function score(answers)
+function score(answerJson)
 {
-
+    return gad7.scoreAnswers(answerJson)
 }
 
-function interpret()
+function interpret(score)
 {
-
+    if(score < 4)
+        return "you're good"
+    else if(score >= 4 && score < 7)
+        return "idk maybe take another one"
+    else return "maybe do something"
 }
+
+console.log(question());
+
+console.log(score({
+    answers: [
+        "Several days",
+        "Over half the days",
+        "Not at all sure",
+        "Nearly every day",
+        "Not at all sure",
+        "Several days",
+        "Over half the days"
+    ]
+}))
+
+console.log(interpret(4))
--- questionnaires/gad7/v1/models/resource/questionnaire.js
+++ questionnaires/gad7/v1/models/resource/questionnaire.js
@@ -2,6 +2,41 @@ var Questionnaire = (function ()
 {
 
     /**
+    * 
+    * @param {String} title title of questionnaire
+    * @param {String} prompt Question prompt
+    * @param {Array} questions array of questions
+    * @param {Array} range range of options (ie 1-5 means possible options for each questions is a number from 1 to 5)
+    * @param {Array} rangeLabels labels for the columns
+    */
+    function matrixBuilder(title, prompt, questions, range, rangeLabels)
+    {
+        var questionnaire = {
+            title: title,
+            prompt: prompt,
+            questions: []
+        }
+
+        for (var question in questions)
+        {
+            questionnaire.questions.push({
+                question: questions[question],
+                options: rangeLabels,
+                option_scores: (() => {
+                    let scores = {};
+                    for (let i = range[0]; i <= range[1]; i += 1) 
+                    {
+                        scores[rangeLabels[i]] = i
+                    }
+                    
+                    return scores;
+                })()
+            })
+        }
+        this.questionnaire = questionnaire
+    }
+
+    /**
      * Questionnaire class
      * 
      * @param {String} type 
@@ -9,7 +44,7 @@ var Questionnaire = (function ()
     function Questionnaire(type, scoreHandler, interpretationHandler)
     {
         this.type = type
-        this.json = null;
+        this.questionnaire = null;
         this.scorer = scoreHandler;
         this.interpreter = interpretationHandler;
     }
@@ -19,41 +54,7 @@ var Questionnaire = (function ()
         var builderFunction = new Function();
         if(this.type === "matrix")
         {
-            /**
-             * 
-             * @param {String} title title of questionnaire
-             * @param {String} prompt Question prompt
-             * @param {Array} questions array of questions
-             * @param {Array} range range of options (ie 1-5 means possible options for each questions is a number from 1 to 5)
-             * @param {Array} rangeLabels labels for the columns
-             */
-            builderFunction = (title, prompt, questions, range, rangeLabels) => 
-            {
-                var json = {
-                    title: title,
-                    prompt: prompt,
-                    questions: []
-                }
-
-                for(var question in questions)
-                {
-                    json.questions.push({
-                        question: questions[question],
-                        options: rangeLabels,
-                        option_scores: (()=>
-                        {
-                            let scores = [];
-                            for(let i = range[0]; i <= range[1]; i += 1)
-                            {
-                                scores.push(i);
-                            }
-                            return scores;
-                        })()
-                    })
-                }
-                
-                this.json = json;
-            }
+            builderFunction = matrixBuilder.bind(this)
         }
         
         return {
@@ -61,9 +62,24 @@ var Questionnaire = (function ()
         }
     }
 
+    Questionnaire.prototype.getAnswerScore = function(questionNumber, answer)
+    {
+        return this.questionnaire.questions[questionNumber].option_scores[answer]
+    }
+
     Questionnaire.prototype.getJson = function()
     {
-        return JSON.stringify(this.json, null, 4)
+        return JSON.stringify(this.questionnaire, null, 4)
+    }
+
+    Questionnaire.prototype.scoreAnswers = function(answers)
+    {
+        return this.scorer(answers)
+    }
+
+    Questionnaire.prototype.interpret = function(score)
+    {
+        return this.interpreter(score)
     }
 
     return Questionnaire;
